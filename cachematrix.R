cc1287f163ec34b3ca8b5a9e3910efd51ea9237c
mahnoushch <66239709+mahnoushch@users.noreply.github.com>
2022-03-15 09:47:11 +0330

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,32 +4,12 @@
 ## then I changed every reference to "mean" to "solve"
 
 makeCacheMatrix <- function(x = matrix()) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  
 
 }
 
 ##
 ## Same here, changed "mean" to "solve" and "m" to "s"
 
-cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
-        
+
 }
