55fe0195db35b8008d7f149e68da21eb1f277fe9
mahnoushch <66239709+mahnoushch@users.noreply.github.com>
2022-03-16 22:49:35 +0330

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,34 @@
-setwd('C:/Users/rubind1/Documents/Coursera-R')
-##
-## I simply set the input x as a matrix
-## and then set the solved value "s" as a null
-## then I changed every reference to "mean" to "solve"
-
+## There are set,get,setsolve, getsolve in this function 
+##To calculate inverst for non squared as well as squard natrices, used labrary(MAss)
+library(mass)
 makeCacheMatrix <- function(x = matrix()) {
-        s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
-
+        #initializing inverst as null
+        s <- NULL        
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x #fucntion to get Matrix x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+        
 }
 
 
-##
-## Same here, changed "mean" to "solve" and "m" to "s"
+##this function is to get tha cache data
 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { #function to get cach data
         s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+        if(!is.null(s)) { #if inverst is Null
+                message("getting inversed matrix")
+                return(s) #return Iverst Value
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s ##return inverse of x
 }
